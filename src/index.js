5e15e6ffac700e12bfc71431f447173f354c962f
Jamie Dixon <jamie@jamie-dixon.co.uk>
2016-07-15 16:31:36 +0100

    Moved index file to root of folder

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import LifecycleComponent from 'src/component';
-import applyLifecyle from 'src/applyLifecycle'
-import connectWithLifecycle from 'src/redux-helpers/lifecyle-connect';
-
-export { LifecycleComponent };
-export { applyLifecyle };
-export { connectWithLifecycle };
